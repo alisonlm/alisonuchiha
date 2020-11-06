a447066bd41008ecb336192a6789f349d93b8af3
alisonlm <alisonlm97@hotmail.com>
2020-11-06 05:11:18 -0300

    Update Header.js
    0.5 Add config Twitch

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,10 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import "bootstrap/dist/js/bootstrap.min.js";
 import AwesomeSlider from 'react-awesome-slider';
 import CoreStyles from "./css/styles.scss";
 import AnimationStyles from './css/fold-out-animation.scss';
 import ReactPlayer from 'react-player/youtube';
-//import "./css/styles.css";
-// import 'react-awesome-slider/dist/styles.css';
+
 
 
 
@@ -64,7 +61,7 @@ const Header = props => (
             <iframe src="https://player.twitch.tv/?video=780696746&parent=localhost" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe>
 <iframe frameborder="0"
         scrolling="yes"
-        src="https://www.twitch.tv/embed/alisonuchiha88/chat?parent=localhost"
+        src="https://www.twitch.tv/embed/alisonuchiha88/chat?parent=http://alisonuchiha.online"
         height="378"
         width="620">
 </iframe>
