be125ee61b0fd438cc5832ac6a594456313dd6f4
alisonlm <alisonlm97@hotmail.com>
2020-11-06 05:19:54 -0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -58,7 +58,7 @@ const Header = props => (
     </div>
 <div class="box">
 
-            <iframe src="https://player.twitch.tv/?video=780696746&parent=localhost" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe>
+            <iframe src="https://player.twitch.tv/?video=780696746&parent=alisonuchiha.site" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe>
 <iframe frameborder="0"
         scrolling="yes"
         src="https://www.twitch.tv/embed/alisonuchiha88/chat?parent=alisonuchiha.site"
